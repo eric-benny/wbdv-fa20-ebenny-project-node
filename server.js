2421297504d97b254049989fe895c3638f09a192
eric-benny <59672346+eric-benny@users.noreply.github.com>
2020-12-13 19:52:01 -0500

    secure cookies

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(session({
                     resave: false,
                     saveUninitialized: true,
                     secret: 'any string',
-                    cookie: { sameSite: 'none' }
+                    cookie: { sameSite: 'none', secure: true }
                 }));
 
 const mongoose = require('mongoose');
@@ -18,9 +18,20 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// app.use(session({
+//                     resave: false,
+//                     saveUninitialized: true,
+//                     secret: 'any string'
+//                 }));
+//
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/traveler',
+//                  {useNewUrlParser: true,
+//                      useUnifiedTopology: true});
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-               "http://localhost:3001");
+               "*");
     res.header("Access-Control-Allow-Headers",
                "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
