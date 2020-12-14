2a5eae37eeda293a704bdd22efff39d9a38fc50f
eric-benny <59672346+eric-benny@users.noreply.github.com>
2020-12-13 20:01:42 -0500

    local app test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(session({
                     resave: false,
                     saveUninitialized: true,
                     secret: 'any string',
-                    cookie: { sameSite: 'none', secure: true }
+                    cookie: { sameSite: 'none', secure: false }
                 }));
 
 const mongoose = require('mongoose');
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-               "*");
+               "http://localhost:3000");
     res.header("Access-Control-Allow-Headers",
                "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
