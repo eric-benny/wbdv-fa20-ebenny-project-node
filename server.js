edd4f36fa12791ec0371674dfdc6e15ed78050cd
eric-benny <59672346+eric-benny@users.noreply.github.com>
2020-12-14 23:16:14 -0500

    switch to remote

--- server.js
+++ server.js
@@ -2,40 +2,40 @@ const express = require("express");
 const app = express();
 
 const session = require('express-session');
-// app.use(session({
-//                     resave: false,
-//                     saveUninitialized: true,
-//                     secret: 'any string',
-//                     proxy: true,
-//                     cookie: { sameSite: 'none', secure: true }
-//                 }));
-//
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://ebenny:ebenny123@wbdv-fa20-ebenny-projec.afo3z.mongodb.net/traveler?retryWrites=true&w=majority',
-//                  {useNewUrlParser: true,
-//                      useUnifiedTopology: true});
-//
-
 app.use(session({
                     resave: false,
                     saveUninitialized: true,
-                    secret: 'any string'
+                    secret: 'any string',
+                    proxy: true,
+                    cookie: { sameSite: 'none', secure: true }
                 }));
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/traveler',
+mongoose.connect('mongodb+srv://ebenny:ebenny123@wbdv-fa20-ebenny-projec.afo3z.mongodb.net/traveler?retryWrites=true&w=majority',
                  {useNewUrlParser: true,
                      useUnifiedTopology: true});
+
+
+// app.use(session({
+//                     resave: false,
+//                     saveUninitialized: true,
+//                     secret: 'any string'
+//                 }));
+//
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/traveler',
+//                  {useNewUrlParser: true,
+//                      useUnifiedTopology: true});
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // https://wbdv-fa20-ebenny-project-react.herokuapp.com
-//
+// http://localhost:3001
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-               "http://localhost:3001");
+               "https://wbdv-fa20-ebenny-project-react.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
                "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
